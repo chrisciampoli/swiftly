9374990dc76dc8c279f879ec0e3e66abb3904f8b
Chris Ciampoli <chris@meetsoci.com>
2014-12-21 23:19:27 -0800

    Updates to add the different views needed for admin role

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -11,52 +11,63 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class AdminController {
 
     /**
      * @Route("/admin/dashboard")
      * @param Request $request
+     * @return array
+     * @Template("AppBundle:Admin:dashboard.html.twig")
      */
     public function dashboardAction(Request $request)
     {
-        echo "Found me!";
+        return [];
     }
 
     /**
      * @Route("/admin/reports")
      * @param Request $request
+     * @return array
+     * @Template("AppBundle:Admin:reports.html.twig")
      */
     public function reportsAction(Request $request)
     {
-
+        return [];
     }
 
     /**
      * @Route("/admin/clients")
      * @param Request $request
+     * @return array
+     * @Template("AppBundle:Admin:clients.html.twig")
      */
     public function clientsAction(Request $request)
     {
-
+        return [];
     }
 
     /**
      * @Route("/admin/users")
      * @param Request $request
+     * @return array
+     * @Template("AppBundle:Admin:users.html.twig")
      */
     public function usersAction(Request $request)
     {
-
+        return [];
     }
 
     /**
      * @Route("/admin/invoices")
      * @param Request $request
+     * @return array
+     * @Template("AppBundle:Admin:invoices.html.twig")
      */
     public function invoicesAction(Request $request)
     {
-
+        return [];
     }
 
 } 
